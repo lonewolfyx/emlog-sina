431fea19098b37e34b069497aedfda92d28bf99d
lonewolfyx <877085893@qq.com>
2015-09-03 11:48:34 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+define( "WB_AKEY" , 'App Key' );
+define( "WB_SKEY" , 'App Secret' );
+define( "WB_CALLBACK_URL" , 'http://youdonaim/callback.php' );
+
+?>
